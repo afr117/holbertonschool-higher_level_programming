b164b92e648c4d3170a395d84e9c9cd7f5c749e1
afr117 <6666@holbertonstudents.com>
2023-09-05 15:02:11 -0700

    update

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -2,5 +2,5 @@
 
 text_to_print = "Programming is like building a multilingual puzzle";
 
-print(f'"{"Programming is like building a multilingual puzzle/n"}"')
+print(f'"{"Programming is like building a multilingual puzzle"/n}"')
 
