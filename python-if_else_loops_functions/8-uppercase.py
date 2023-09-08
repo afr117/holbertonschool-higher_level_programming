df32c3a18b13d5c7cf00684aff23b0300a0cfd14
afr117 <6666@holbertonstudents.com>
2023-09-07 17:58:42 -0700

    correct output8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+def uppercase(str):
+    """Print a string in uppercase followed by a new line"""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = chr(ord(char) - 32)
+        print("{}".format(char), end="")
+    print()
