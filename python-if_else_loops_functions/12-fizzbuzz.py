9b62fa5cf61fb43771dbe1eef3ed8ef63257237c
afr117 <6666@holbertonstudents.com>
2023-09-07 18:14:26 -0700

    another update12

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,9 @@
 
 def fizzbuzz():
 
+
     """Prints numbers from 1 to 100 with FizzBuzz rules"""
+
 
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
@@ -14,5 +16,7 @@ def fizzbuzz():
         else:
             print(i, end=' ')
 
+
 if __name__ == "__main__":
+
     fizzbuzz()
