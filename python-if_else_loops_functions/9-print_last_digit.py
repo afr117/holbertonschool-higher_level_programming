969461f70518fb669baf175dda5e812676de60e4
afr117 <6666@holbertonstudents.com>
2023-09-07 18:00:59 -0700

    output

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+
+def print_last_digit(number):
+    """Prints and returns the last digit of a number"""
+    last_digit = abs(number) % 10
+    print("{}".format(last_digit), end="")
+    return last_digit
