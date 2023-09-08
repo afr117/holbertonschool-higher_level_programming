cf91eaa4f945a27d792c8e8b7424006cebbf15b4
afr117 <6666@holbertonstudents.com>
2023-09-07 18:25:28 -0700

    update 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import re
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[39:67] + str[107:112] + str[0:6])
+print(re.findall(r'object-oriented programming', str)[0])
