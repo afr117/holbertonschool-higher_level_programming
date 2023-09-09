22525db630d6f48ea81595de6561d51cb05f4d85
afr117 <6666@holbertonstudents.com>
2023-09-09 10:08:15 -0700

    adding $ to print

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -4,7 +4,7 @@ def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
             print("{:d}".format(num), end=" " if i < len(row) - 1 else "")
-        print()
+        print("$")
 
 # Example usage
 if __name__ == "__main__":
