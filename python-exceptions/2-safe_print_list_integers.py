a774b8cb3440017c0b181166811adbb502a17e0c
afr117 <6666@holbertonstudents.com>
2023-09-09 11:11:55 -0700

    test0-2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,8 +7,8 @@ def safe_print_list_integers(my_list=[], x=0):
             if isinstance(my_list[i], int):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
-        except IndexError:
-            break
+        except (ValueError, TypeError):
+            pass
     print()
     return count
 
