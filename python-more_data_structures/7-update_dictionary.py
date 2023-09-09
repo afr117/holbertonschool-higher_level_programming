431e535d33ba250842eba32f933acd16ddcf3841
afr117 <6666@holbertonstudents.com>
2023-09-09 07:36:17 -0700

    update file6

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,23 +1,30 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    # Update the value of an existing key or add a new key-value pair
     a_dictionary[key] = value
 
+    return a_dictionary
+def print_sorted_dictionary(a_dictionary):
+    sorted_keys = sorted(a_dictionary.keys())
+
+    for key in sorted_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
 # Example usage
+
 if __name__ == "__main__":
     a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-    
     new_dict = update_dictionary(a_dictionary, 'language', "Python")
     print_sorted_dictionary(new_dict)
+
     print("--")
     print_sorted_dictionary(a_dictionary)
-
+    
     print("--")
     print("--")
-
+    
     new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
     print_sorted_dictionary(new_dict)
+
     print("--")
     print_sorted_dictionary(a_dictionary)
 
