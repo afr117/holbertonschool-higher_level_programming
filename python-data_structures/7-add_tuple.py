7bf90fcaa27def12177274296651b47e4472afc1
afr117 <6666@holbertonstudents.com>
2023-09-09 10:32:43 -0700

    correct output7

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Ensure both tuples have at least two elements by extending with 0 if necessary
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+
+    # Calculate the sum of corresponding elements in the tuples
+    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+
+    return result_tuple
+
+# Example usage
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
+
