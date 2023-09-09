4a49307aa2f1976c9b200f6198d550ca414d993c
afr117 <6666@holbertonstudents.com>
2023-09-08 17:11:19 -0700

    update file7

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-string1, string2, string3 = word[0:3], word[7:9], word[1:8]
-word_first_3, word_last_2, middle_word = string1, string2, string3
-
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
