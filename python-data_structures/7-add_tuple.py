253bf543409e4f9e2e483dba517171f4c32703d5
afr117 <6666@holbertonstudents.com>
2023-09-10 14:36:30 -0700

    test1-7

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,8 +3,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Extract the first and second elements from each tuple
     a1, a2 = tuple_a + (0, 0)
     b1, b2 = tuple_b + (0, 0)
+
     # Calculate the sum of the corresponding elements
     sum_1 = a1 + b1
     sum_2 = a2 + b2
+
     # Return the result as a new tuple
     return (sum_1, sum_2)
