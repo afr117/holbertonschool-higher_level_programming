d471f9665b1273a88f27153f6412911d76977efe
afr117 <6666@holbertonstudents.com>
2023-09-10 14:33:31 -0700

    actual allfiles update

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python3
-
 def delete_at(my_list=[], idx=0):
     # Check if idx is negative or out of range
     if idx < 0 or idx >= len(my_list):
         return my_list
-
     # Remove the element at the specified index in-place
     del my_list[idx]
-
     return my_list
-
-# Example usage
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    idx = 3
-    new_list = delete_at(my_list, idx)
-    print(new_list)
-    print(my_list)
