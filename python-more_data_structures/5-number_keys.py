e4dbb1a180da04f28c50b8a5dd8a632db01ede53
afr117 <6666@holbertonstudents.com>
2023-09-10 13:43:22 -0700

    updated5

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-
 def number_keys(a_dictionary):
-    # Use len() to count the number of keys in the dictionary
+    # Use len() to count number of keys in the dictionary
     num_keys = len(a_dictionary.keys())
     return num_keys
-
-# Example usage
-if __name__ == "__main__":
-    a_dictionary = { 'language': "C", 'number': 13, 'track': "Low level" }
-    nb_keys = number_keys(a_dictionary)
-    print("Number of keys: {:d}".format(nb_keys))
-
