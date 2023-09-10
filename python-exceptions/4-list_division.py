f8d481364988196cd5b262e744e88af1c7a4dbb8
afr117 <6666@holbertonstudents.com>
2023-09-10 14:21:37 -0700

    update 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     for i in range(list_length):
@@ -17,18 +16,3 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             result.append(quotient)
     return result
-
-# Example usage
-if __name__ == "__main__":
-    my_l_1 = [10, 8, 4]
-    my_l_2 = [2, 4, 4]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
-    print("--")
-
-    my_l_1 = [10, 8, 4, 4]
-    my_l_2 = [2, 0, "H", 2, 7]
-    result = list_division(my_l_1, my_l_2, max(len(my_l_1), len(my_l_2)))
-    print(result)
-
