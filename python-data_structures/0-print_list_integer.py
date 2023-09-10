05e172d78af824b1c5d2babefff24b70a201fc53
afr117 <6666@holbertonstudents.com>
2023-09-10 14:25:10 -0700

    updated 0

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,7 +2,3 @@
 def print_list_integer(my_list=[]):
     for item in my_list:
         print("{:d}".format(item))
-# Example usage
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    print_list_integer(my_list)
