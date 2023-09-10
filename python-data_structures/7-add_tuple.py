26142d7b59f90ebe237aedb96d10fa29103c68f0
afr117 <6666@holbertonstudents.com>
2023-09-10 14:41:03 -0700

    remove emptylines

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,11 +4,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a1 = tuple_a[0] if len(tuple_a) > 0 else 0
     a2 = tuple_a[1] if len(tuple_a) > 1 else 0
     b1 = tuple_b[0] if len(tuple_b) > 0 else 0
-    b2 = tuple_b[1] if len(tuple_b) > 1 else 0
-    
+    b2 = tuple_b[1] if len(tuple_b) > 1 else 0 
     # Calculate the sums
     sum1 = a1 + b1
     sum2 = a2 + b2
-    
     # Return a tuple with the sums
     return (sum1, sum2)
