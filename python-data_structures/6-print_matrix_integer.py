7402cf2605bbbb0a7ee84ade7447017d86089885
afr117 <6666@holbertonstudents.com>
2023-09-10 14:31:49 -0700

    updated all files

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,17 +1,6 @@
 #!/usr/bin/python3
-
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, num in enumerate(row):
             print("{:d}".format(num), end=" " if i < len(row) - 1 else "")
         print()
-
-# Example usage
-if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
-
-    print_matrix_integer(matrix)
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,18 +1,10 @@
 #!/usr/bin/python3
-
 def add_tuple(tuple_a=(), tuple_b=()):
-    # Ensure both tuples have at least two elements by extending with 0 if necessary
-    tuple_a = tuple_a + (0, 0)
-    tuple_b = tuple_b + (0, 0)
-    result_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-
-    return result_tuple
-
-if __name__ == "__main__":
-    tuple_a = (1, 89)
-    tuple_b = (88, 11)
-    new_tuple = add_tuple(tuple_a, tuple_b)
-
-    print(new_tuple)
-    print(add_tuple(tuple_a, (1, )))
-    print(add_tuple(tuple_a, ()))
+    # Extract the first and second elements from each tuple
+    a1, a2 = tuple_a + (0, 0)
+    b1, b2 = tuple_b + (0, 0)
+    # Calculate the sum of the corresponding elements
+    sum_1 = a1 + b1
+    sum_2 = a2 + b2
+    # Return the result as a new tuple
+    return (sum_1, sum_2)
