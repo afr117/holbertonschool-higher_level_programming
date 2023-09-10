f11bb1ae6ec1677e9402f61bacb404944b16215f
afr117 <6666@holbertonstudents.com>
2023-09-10 14:49:14 -0700

    remove comments10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    # Create a new list of booleans to store whether each element is divisible by 2
     result = []
-    # Iterate through the elements in the input list
     for number in my_list:
-        # Check if the number is divisible by 2
         if number % 2 == 0:
             result.append(True)
         else:
