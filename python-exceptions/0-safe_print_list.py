7d386cbc496746425ac5ccf46a3990cbc3c68664
afr117 <6666@holbertonstudents.com>
2023-09-10 14:13:15 -0700

    ipdate1-0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,14 +7,6 @@ def safe_print_list(my_list=[], x=0):
             count += 1
         print()
         return count
-    except IndexError:
+    except (TypeError, IndexError):
         print()
         return count
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5]
-    nb_print = safe_print_list(my_list, 2)
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list))
-    print("nb_print: {:d}".format(nb_print))
-    nb_print = safe_print_list(my_list, len(my_list) + 2)
-    print("nb_print: {:d}".format(nb_print))
