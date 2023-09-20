056443d837de9754925ef00a522e930bc0b9e394
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 15:11:07 -0700

    testlines2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -6,25 +6,26 @@ This module defines a function to divide all elements of a matrix by a given div
 
 def matrix_divided(matrix, div):
     """
-    Divides matrix elements by a given divisor.
-    Args:
-        matrix (list of lists): The matrix to be divided.
-        div (int or float): The divisor.
-    Returns:
-        New matrix with elements divided by div, rounded to 2 decimal places.
-    Raises:
-        TypeError if matrix not list of lists of integers/floats or if div not a number (int or float).
-    ValueError: If matrix rows not have the same size.
-        ZeroDivisionError: If div is 0.
+Matrix division function.
 
-    Example:
-        >>> matrix = [
-        ...     [1, 2, 3],
-        ...     [4, 5, 6]
-        ... ]
-        >>> matrix_divided(matrix, 3)
-        [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
-    """
+Divides matrix elements by a divisor.
+Args:
+    matrix (list of lists): The matrix to be divided.
+    div (int or float): The divisor.
+Returns:
+    New matrix with elements divided by div, rounded to 2 decimal places.
+Raises:
+    TypeError if matrix is not list of lists of integers/floats, or if div is not a number (int or float).
+    ValueError: If matrix rows do not have the same size.
+    ZeroDivisionError: If div is 0.
+Example:
+    >>> matrix = [
+    ...     [1, 2, 3],
+    ...     [4, 5, 6]
+    ... ]
+    >>> matrix_divided(matrix, 3)
+    [[0.33, 0.67, 1.0], [1.33, 1.67, 2.0]]
+"""
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     
