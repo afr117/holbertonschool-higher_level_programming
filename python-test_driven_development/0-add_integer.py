4d0b80c1b67a5f3462c9dedc0f694b97b55fc781
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-19 18:12:42 -0700

    better

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,27 +1,22 @@
 #!/usr/bin/python3
 """
-This module defines a function that adds two integers.
+
+Additional comment: This function is a simple example of integer addition.
+
 """
-
-
 def add_integer(a, b=98):
     """
-    Add two integers.
-
-    :param a: The first integer.
-    :param b: The second integer (default is 98).
-    :type a: int
-    :type b: int
-    :return: The sum of a and b.
-    :rtype: int
+    This function adds two integers.
+    Args:
+        a (int): The first integer.
+        b (int, optional): The second integer (default is 98).
+    Returns:
+        int: The sum of the two integers.
+    Raises:
+        TypeError: If either 'a' or 'b' is not an integer.
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
