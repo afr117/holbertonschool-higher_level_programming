1758b849d525c30d692ee13b15a3d1a9b411f440
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-19 18:56:52 -0700

    test

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,17 +6,16 @@ Additional comment: This function is a simple example of integer addition.
 """
 def add_integer(a, b=98):
     """
-    This function adds two integers.
-    Args:
-        a (int): The first integer.
-        b (int, optional): The second integer (default is 98).
+    Args: a (int or float): The first integer or float: b (int or float, optional): Second integer or float (default 98).
     Returns:
-        int: The sum of the two integers.
+        int: The addition of 'a' and 'b' as integers
     Raises:
-        TypeError: If either 'a' or 'b' is not an integer.
+        TypeError: If 'a' or 'b' is not an integer or float.
     """
     if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
+
+
+raise TypeError("a must be an integer or b must be an integer")
     if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+        raise TypeError("a must be an integer or b must be an integer")
     return int(a) + int(b)
