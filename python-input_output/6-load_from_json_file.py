60860562195b4e2cad84c34baf30420d8a47ddbf
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-21 16:57:05 -0700

    file6

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Module for loading Python objects from a JSON file.
+"""
+
+import json
+
+def load_from_json_file(filename):
+    """
+    Creates an object from a JSON file.
+
+    Args:
+        filename: The name of the JSON file to load.
+
+    Returns:
+        object: The Python object loaded from the JSON file.
+    """
+    with open(filename, 'r', encoding='utf-8') as file:
+        return json.load(file)
+
+if __name__ == "__main__":
+    load_from_json_file()
+
