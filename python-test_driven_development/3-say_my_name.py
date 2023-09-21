736a60c00a1dc4ca65313a63a0ae81c1827c6c73
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 17:18:22 -0700

    test5-3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -23,10 +23,10 @@ def say_my_name(first_name, last_name=""):
         >>> say_my_name("Bob")
         My name is Bob.
     """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
+    if not isinstance(first_name, str) or (last_name and not isinstance(last_name, str)):
         raise TypeError("first_name must be a string or last_name must be a string")
     if last_name:
-        print("My name is {} {}".format(first_name, last_name))
+        print("My name is {} {}.".format(first_name, last_name))
     else:
         print("My name is {} ".format(first_name))
 
@@ -38,3 +38,4 @@ if __name__ == "__main__":
         say_my_name(12, "White")
     except Exception as e:
         print(e)
+
