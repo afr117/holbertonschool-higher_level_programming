1275d2a2c1d1575ccb0f4297253b47a4272e90ff
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 17:09:42 -0700

    test4-3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -28,7 +28,7 @@ def say_my_name(first_name, last_name=""):
     if last_name:
         print("My name is {} {}".format(first_name, last_name))
     else:
-        print("My name is {}".format(first_name))
+        print("My name is {} {}".format(first_name))
 
 if __name__ == "__main__":
     say_my_name("John", "Smith")
