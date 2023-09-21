b32afcbd8f234172f9547669d2c07e32c0f0c694
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 20:51:32 -0700

    test23

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -5,6 +5,9 @@ testing file comments 1, 2, 3
 
 
 def matrix_mul(m_a, m_b):
+    """
+    Matrix multiplication function
+    """
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise TypeError("m_a must be a list or m_b must be a list")
     if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
