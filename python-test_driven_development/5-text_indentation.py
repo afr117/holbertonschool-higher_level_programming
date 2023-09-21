1e4a8d6667b271024f7a25621e5892719e71741c
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 20:04:46 -0700

    test6-5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 This module provides various functions for performing calculations.
 You can use these functions to add, subtract, multiply, and divide numbers. 
@@ -8,7 +7,7 @@ You can use these functions to add, subtract, multiply, and divide numbers.
 
 def text_indentation(text):
     """
-    Prints a text with 2 new lines after each '.', '?', and ':' character.
+    Prints a text with 2 new lines after each of these characters: ., ? and :
 
     Args:
         text (str): The input text.
@@ -17,66 +16,25 @@ def text_indentation(text):
         TypeError: If text is not a string.
 
     Example:
-        >>> text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-        Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-        Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-        Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-        Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-        rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-        stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-        cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-        beatiorem! Iam ruinas videres""")
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        $
-        $
-        Quonam modo?
-        $
-        $
-        Utrum igitur tibi litteram videor an totas paginas commovere?
-        $
-        $
-        Non autem hoc:
-        $
-        $
-        igitur ne illud quidem.
-        $
-        $
-        Fortasse id optimum, sed ubi illud:
-        $
-        $
-        Plus semper voluptatis?
-        $
-        $
-        Teneo, inquit, finem illi videri nihil dolere.
-        $
-        $
-        Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum rationi oboediens.
-        $
-        $
-        Si id dicis, vicimus.
-        $
-        $
-        Inde sermone vario sex illa a Dipylo stadia confecimus.
-        $
-        $
-        Sin aliud quid voles, postea.
-        $
-        $
-        Quae animi affectio suum cuique tribuens atque hanc, quam dico.
-        $
-        $
-        Utinam quidem dicerent alium alio beatiorem! Iam ruinas videres
+        >>> text_indentation("Hello. How are you?")
+        Hello.
+        How are you?
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    buffer = ""
+    separators = [".", "?", ":"]
+    lines = []
+
+    line = ""
     for char in text:
-        buffer += char
-        if char in ".?:":
-            print(buffer.strip())
-            print()
-            buffer = ""
+        line += char
+        if char in separators:
+            lines.append(line.strip())
+            line = ""
 
-    print(buffer.strip())
+    if line:
+        lines.append(line.strip())
 
+    for line in lines:
+        print(line)
