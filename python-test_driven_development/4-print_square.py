cf9bba0f69e25f09784581462d2d5c5e4f628811
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 19:23:18 -0700

    test2-4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-test
+This module defines a function to print a square with the character #.
 """
-
 
 def print_square(size):
     """
@@ -12,7 +11,7 @@ def print_square(size):
         size (int): The size length of the square.
 
     Raises:
-        TypeError: If size is not an integer or if it's a float less than 0.
+        TypeError: If size is not an integer.
         ValueError: If size is less than 0.
 
     Example:
@@ -21,40 +20,27 @@ def print_square(size):
         ####
         ####
         ####
-        >>> print_square(10)
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        ##########
-        >>> print_square(0)
-        >>> print_square(1)
-        #
-        >>> print_square(-1)
-        Traceback (most recent call last):
-            ...
-        ValueError: size must be >= 0
     """
     if not isinstance(size, int):
-        if isinstance(size, float) and size >= 0:
-            size = int(size)
-        else:
-            raise TypeError("size must be an integer")
-
+        raise TypeError("size must be an integer")
+    
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    if size == 0:
-        return
-
+    
     for _ in range(size):
         print("#" * size)
 
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+    print_square(4)
+    print("")
+    print_square(10)
+    print("")
+    print_square(0)
+    print("")
+    print_square(1)
+    print("")
+    try:
+        print_square(-1)
+    except Exception as e:
+        print(e)
+    print("")
