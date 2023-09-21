1639632ea3bfed3eba29ef3188da1e298133d523
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 20:22:28 -0700

    test7-5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,24 +4,25 @@ This module provides various functions for performing calculations.
 You can use these functions to add, subtract, multiply, and divide numbers. 
 """
 
-
 def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
-    text = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
-    
-    lines = text.splitlines()
-    formatted_text = '\n'.join(line.strip() for line in lines if line.strip())
+
+    chars = ['.', '?', ':']
+    new_text = ""
+    add_newline = False
+
+    for char in text:
+        new_text += char
+        if char in chars:
+            new_text += '\n\n'
+            add_newline = True
+        else:
+            add_newline = False
+
+    lines = new_text.split('\n')
+    formatted_lines = [line.strip() for line in lines if line.strip()]
+    formatted_text = '\n'.join(formatted_lines)
+
     print(formatted_text)
 
-if __name__ == "__main__":
-    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
-Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
-Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
-Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
-Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
-rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
-stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
-cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
-beatiorem! Iam ruinas videres""")
