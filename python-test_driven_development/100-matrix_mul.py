e4398abff67c09a9ea30114eabecba0c5d583e9a
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 21:11:29 -0700

    update

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -4,25 +4,28 @@ testing file comments 1, 2, 3
 """
 
 
-def matrix_mul(m_a, m_b):
+    def matrix_mul(m_a, m_b):
     """
     Matrix multiplication function
     """
-    if not isinstance(m_a, list) or not isinstance(m_b, list):
-        raise TypeError("m_a must be a list and m_b must be a list")
-    
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_a must be a list of lists and m_b must be a list of lists")
-    
-    if not m_a or any(not row for row in m_a) or not m_b or any(not row for row in m_b):
-        raise ValueError("m_a can't be empty and m_b can't be empty")
-    
-    if any(not isinstance(num, (int, float)) for row in m_a for num in row) or any(not isinstance(num, (int, float)) for row in m_b for num in row):
-        raise TypeError("m_a should contain only integers or floats and m_b should contain only integers or floats")
-    
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if not m_a or any(not row for row in m_a):
+        raise ValueError("m_a can't be empty")
+    if not m_b or any(not row for row in m_b):
+        raise ValueError("m_b can't be empty")
+    if any(not isinstance(num, (int, float)) for row in m_a for num in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if any(not isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_b should contain only integers or floats")
     if any(len(row) != len(m_a[0]) for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
-        raise TypeError("each row of m_a must be of the same size and each row of m_b must be of the same size")
-    
+        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
