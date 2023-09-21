5a3c3a1595ece4dd2aaed66294167da2f7397666
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-21 16:14:48 -0700

    test1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -17,6 +17,5 @@ def write_file(filename="", text=""):
     """
     with open(filename, 'w', encoding='utf-8') as file:
         return file.write(text)
-
 if __name__ == "__main__":
     write_file()
