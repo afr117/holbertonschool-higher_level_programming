151f2639c8428f608b76da35cd018924eb0fffd1
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 18:23:52 -0700

    test8-3

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,25 +2,27 @@
 """
 test
 """
+
+
 def say_my_name(first_name, last_name=""):
     """
-    Prints "My name is <first name> <last name>."
+Prints 'My name is <first name> <last name>.'
 
-    Args:
-        first_name (str): The first name.
-        last_name (str, optional): The last name.
+Args:
+    first_name (str): The first name.
+    last_name (str, optional): The last name.
 
-    Raises:
-        TypeError: If first_name is not a string or if last_name is provided and is not a string.
+Raises:
+    TypeError: If first_name not string or if last_name not a string when provided.
 
-    Example:
-        >>> say_my_name("John", "Smith")
-        My name is John Smith.
-        >>> say_my_name("Walter", "White")
-        My name is Walter White.
-        >>> say_my_name("Bob")
-        My name is Bob.
-    """
+Examples:
+    >>> say_my_name('John', 'Smith')
+    My name is John Smith.
+    >>> say_my_name('Walter', 'White')
+    My name is Walter White.
+    >>> say_my_name('Bob')
+    My name is Bob.
+"""
     if not isinstance(first_name, str) or (last_name and not isinstance(last_name, str)):
         raise TypeError("first_name must be a string" if not isinstance(first_name, str) else "last_name must be a string")
     
