0b6a277d180ffefe60334d05039650fc891a82c7
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 20:16:50 -0700

    test6-5

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,35 +6,22 @@ You can use these functions to add, subtract, multiply, and divide numbers.
 
 
 def text_indentation(text):
-    """
-    Prints a text with 2 new lines after each of these characters: ., ? and :
-
-    Args:
-        text (str): The input text.
-
-    Raises:
-        TypeError: If text is not a string.
-
-    Example:
-        >>> text_indentation("Hello. How are you?")
-        Hello.
-        How are you?
-    """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+    
+    text = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
+    
+    lines = text.splitlines()
+    formatted_text = '\n'.join(line.strip() for line in lines if line.strip())
+    print(formatted_text)
 
-    separators = [".", "?", ":"]
-    lines = []
-
-    line = ""
-    for char in text:
-        line += char
-        if char in separators:
-            lines.append(line.strip())
-            line = ""
-
-    if line:
-        lines.append(line.strip())
-
-    for line in lines:
-        print(line)
+if __name__ == "__main__":
+    text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+beatiorem! Iam ruinas videres""")
