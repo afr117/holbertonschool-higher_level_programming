4ae17989fccab9ca274d287db9fcc7d890af9491
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-20 19:40:56 -0700

    test3-4

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -45,17 +45,12 @@ def print_square(size):
             size = int(size)
         else:
             raise TypeError("size must be an integer")
-
     if size < 0:
         raise ValueError("size must be >= 0")
-
     if size == 0:
         return
-
     for _ in range(size):
         print("#" * size)
-
     if __name__ == "__main__":
-
     import doctest
     doctest.testmod()
