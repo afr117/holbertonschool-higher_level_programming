096679332c684dc30a6d1aa6155c6a6bfa936f3c
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-21 16:44:27 -0700

    test2-3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -4,6 +4,7 @@ Module for converting objects to JSON strings.
 """
 
 import json
+from collections import OrderedDict
 
 def to_json_string(my_obj):
     """
@@ -15,10 +16,12 @@ def to_json_string(my_obj):
     Returns:
         str: The JSON representation of the object.
     """
-    if isinstance(my_obj, dict):
-        my_obj = [(str(key), value) for key, value in sorted(my_obj.items())]
+    class OrderedEncoder(json.JSONEncoder):
+        def encode(self, obj):
+            return super().encode(obj)
 
-    return json.dumps(my_obj, ensure_ascii=False)
+    ordered_obj = json.loads(json.dumps(my_obj, ensure_ascii=False), object_pairs_hook=OrderedDict)
+    return json.dumps(ordered_obj, ensure_ascii=False, cls=OrderedEncoder)
 
 if __name__ == "__main__":
     to_json_string()
