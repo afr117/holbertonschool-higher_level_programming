6dbd84f005a92e9355a486a189ef5f0a15e52047
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-23 12:07:42 -0700

    file3 correct output perfet file2[D[D[D[D

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that defines the is_kind_of_class function."""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Check if obj is an instance of, or inherited from, the specified class.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to compare obj against.
+
+    Returns:
+        True if obj is an instance of, or inherited from, a_class; otherwise, False.
+    """
+    return isinstance(obj, a_class)
