4cb06ce6ae1382fb7b2de408e9017326c7a43e6b
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-23 12:14:11 -0700

    ffile 6 1 error same as others

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """A class representing base geometry."""
+
+    def area(self):
+        """Raise an Exception with the message 'area() is not implemented'."""
+        raise Exception("area() is not implemented")
