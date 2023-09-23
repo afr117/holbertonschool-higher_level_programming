5e66b707e40e5efbb8ecb5be651a8dd1066d1432
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-23 12:20:55 -0700

    original7 less error still errortho

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,8 +11,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Validate the value as an integer."""
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-
