70b1f6f823f3279d26b816408d2d972a28d4d4eb
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-23 10:42:30 -0700

    file10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python3
+"""Module to define the Student class."""
+
 
 class Student:
+    """A class that defines a student."""
+
     def __init__(self, first_name, last_name, age):
+        """
+        Initialize a new Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+
+        Attributes:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Retrieve a dictionary representation of the Student instance.
+
+        Args:
+            attrs (list, optional): A list of attribute names to include in the dictionary.
+                If provided, only these attributes will be retrieved. Default is None.
+
+        Returns:
+            dict: A dictionary containing the specified attributes of the Student.
+        """
         if attrs is None:
             return self.__dict__
-        return {attr: getattr(self, attr) for attr in sorted(attrs) if hasattr(self, attr)}
-
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
