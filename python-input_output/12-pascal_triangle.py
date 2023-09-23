56c24e3d269f6f34c934e2921c86df9d9612ddea
affr117 <alfredo.figueroa07@yahoo.com>
2023-09-23 11:15:24 -0700

    file12 correctformat

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+12-pascal_triangle
+"""
+
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows in Pascal's Triangle.
+
+    Returns:
+        list: A list of lists representing Pascal's Triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        prev_row = triangle[-1]
+        new_row = [1]
+
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+
+        new_row.append(1)
+        triangle.append(new_row)
+
+    return triangle
+
+if __name__ == "__main__":
+    pass
