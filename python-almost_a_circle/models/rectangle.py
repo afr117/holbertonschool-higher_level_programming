d8edbe849e8f886ef9c40a1089d36b80fcdfb3dc
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-04 16:33:13 -0700

    correct output task2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,98 +1,16 @@
-#!/usr/bin/python3
-
-from base import Base
-
+# models/rectangle.py
+from models.base import Base
 
 class Rectangle(Base):
-    """Rectangle class, inherits from Base."""
+    """Rectangle class inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Start Rectangle instance width, height, and optional x, y, or ID."""
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    @property
-    def width(self):
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Calculate and return the area of the Rectangle."""
-        return self.__width * self.__height
-
-    def display(self):
-        """Print the Rectangle instance with '#' characters."""
-        for _ in range(self.__y):
-            print()
-        for _ in range(self.__height):
-            print(' ' * self.__x + '#' * self.__width)
-
     def __str__(self):
-        """Override str method returns [Rectangle]."""
-        return f"{self.id}{self.__x}/{self.__y}-{self.__width}/{self.__height}"
-
-    def update(self, *args, **kwargs):
-        """Update attributes based on arguments or keyword arguments."""
-        if args:
-            arg_names = ["id", "width", "height", "x", "y"]
-            for i, arg in enumerate(args):
-                setattr(self, arg_names[i], arg)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return a dictionary representation of the Rectangle."""
-        return {
-            'id': self.id,
-            'width': self.__width,
-            'height': self.__height,
-            'x': self.__x,
-            'y': self.__y
-        }
+        """Override the __str__ method to return [Rectangle] instance."""
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}"
