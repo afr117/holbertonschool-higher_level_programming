e41b2191bf712edc2c07f7307b7e0352bc7a3d57
alfredo figueroa <alfredo.figueroa07@yahoo.com>
2023-10-03 18:34:40 -0700

    5

--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -5,24 +5,32 @@ from square import Square
 
 class TestSquare(unittest.TestCase):
     def test_init(self):
-        # Add test cases for Square creation
-        pass
+        s1 = Square(5)
+        self.assertEqual(s1.size, 5)
+        self.assertEqual(s1.x, 0)
+        self.assertEqual(s1.y, 0)
+        self.assertEqual(s1.id, 1)
 
     def test_area(self):
-        # Add test cases for area method
-        pass
+        s2 = Square(3)
+        self.assertEqual(s2.area(), 9)
 
     def test_display(self):
-        # Add test cases for display method
-        pass
+        s3 = Square(2)
+        expected_output = "##\n##\n"
+        self.assertEqual(s3.display(), expected_output)
 
     def test_update(self):
-        # Add test cases for update method
-        pass
+        s4 = Square(1)
+        s4.update(2)
+        self.assertEqual(s4.id, 2)
+        s4.update(2, 3, 4)
+        self.assertEqual(s4.size, 3)
+        self.assertEqual(s4.x, 4)
+        s4.update(y=5, x=6)
+        self.assertEqual(s4.x, 6)
+        self.assertEqual(s4.y, 5)
 
     def test_to_dictionary(self):
-        # Add test cases for to_dictionary method
-        pass
-
-if __name__ == "__main__":
-    unittest.main()
+        s5 = Square(3)
+        expected_dict = {'id': 1, 'size': 3, 'x':
