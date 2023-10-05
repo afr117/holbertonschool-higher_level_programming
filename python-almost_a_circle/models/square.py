be2ac785e81d27d7906f9fd2b7fed16649b80912
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-04 20:57:50 -0700

    task 11 correct out and format

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -30,10 +30,8 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Return string representation of Square."""
-        return "[Square]({}){}/{}-{}".format(
-                self.id, self.x, self.y, self.width
-                )
+        """Returns a string representation of the Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
 
     @property
     def size(self):
@@ -43,13 +41,24 @@ class Square(Rectangle):
     @size.setter
     def size(self, value):
         """Setter for size attribute."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
-    def area(self):
-        """Returns the area of the square."""
-        return self.width * self.height
+    def update(self, *args, **kwargs):
+        """Updates the Square attributes with the provided arguments."""
+        if args:
+            attributes = ["id", "width", "x", "y"]
+            for i in range(len(args)):
+                setattr(self, attributes[i], args[i])
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns the dictionary representation of the Square."""
+        return {
+            'id': self.id,
+            'size': self.width,
+            'x': self.x,
+            'y': self.y
+        }
