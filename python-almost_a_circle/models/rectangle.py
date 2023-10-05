785aa36243537386a6871d0503e281be8e3152ae
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-04 17:49:00 -0700

    pass checks correct out

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -5,6 +5,7 @@ Rectangle module - Contains the Rectangle class
 
 from models.base import Base
 
+
 class Rectangle(Base):
     """
     Rectangle class inherits from Base.
@@ -23,9 +24,9 @@ class Rectangle(Base):
         Args:
             width (int): Width of the rectangle.
             height (int): Height of the rectangle.
-            x (int, optional): X coordinate of the rectangle's position. Defaults to 0.
-            y (int, optional): Y coordinate of the rectangle's position. Defaults to 0.
-            id (int, optional): Unique identifier for the rectangle. Defaults to None.
+            x (int,optional): X coordinate of rectangle position. Defaults 0.
+            y (int,optional): Y coordinate of rectangle position. Defaults 0.
+            id (int,optional): Unique identifier for rectangle. Defaults None.
         """
         super().__init__(id)
         self.width = width
@@ -89,7 +90,9 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
+
 if __name__ == "__main__":
+
     try:
         Rectangle(10, "2")
     except Exception as e:
