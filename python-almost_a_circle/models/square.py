d4f118bbeac8e2d979908ce1ff80422f4a862573
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-04 20:41:20 -0700

    task9 correct format and out

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,33 +1,51 @@
 #!/usr/bin/python3
+"""
+Square module - Contains the Square class
+"""
 
-from rectangle import Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class, inherits from Rectangle."""
+    """
+    Square class inherits from Rectangle class.
+
+    Attributes:
+        size (int): Size of the square.
+        x (int): X coordinate of the square's position.
+        y (int): Y coordinate of the square's position.
+        id (int): Unique identifier for the square.
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize Square instance size, and optional x, y, or ID."""
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): Size of the square.
+            x (int, optional): X coordinate of square position. Defaults to 0.
+            y (int, optional): Y coordinate of square position. Defaults to 0.
+            id (int, optional): Unique identifier for square. Defaults to None.
+        """
         super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return string representation of Square."""
+        return "[Square]({}){}/{}-{}".format(
+                self.id, self.x, self.y, self.width
+                )
 
     @property
     def size(self):
+        """Getter for size attribute."""
         return self.width
 
     @size.setter
     def size(self, value):
+        """Setter for size attribute."""
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """Update attributes based on arguments or keyword arguments."""
-        super().update(*args, **kwargs)
-
-    def to_dictionary(self):
-        """Return a dictionary representation of the Square."""
-        return {
-            'id': self.id,
-            'size': self.width,
-            'x': self.x,
-            'y': self.y
-        }
+    def area(self):
+        """Returns the area of the square."""
+        return self.width * self.height
