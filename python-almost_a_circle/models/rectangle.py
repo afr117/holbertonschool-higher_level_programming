c430e4213d8a96da67f51466f1f2697b08bc2bb1
affr117 <alfredo.figueroa07@yahoo.com>
2023-10-04 18:37:57 -0700

    task4-5 correct out

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -11,10 +11,10 @@ class Rectangle(Base):
     Rectangle class inherits from Base.
 
     Attributes:
-        width (int): Width of rectangle.
-        height (int): Height of rectangle.
-        x (int): X coordinate of rectangle's position.
-        y (int): Y coordinate of rectangle's position.
+        width (int): Width of the rectangle.
+        height (int): Height of the rectangle.
+        x (int): X coordinate of the rectangle's position.
+        y (int): Y coordinate of the rectangle's position.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
@@ -27,7 +27,7 @@ class Rectangle(Base):
             x (int,optional): X coordinate of rectangle position. Defaults 0.
             y (int,optional): Y coordinate of rectangle position. Defaults 0.
             id (int,optional): Unique identifier for rectangle. Defaults None.
-        """
+"""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -56,6 +56,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter for height attribute."""
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
@@ -90,31 +91,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = value
 
-    def area(self):
-        """Calculate and return the area of the rectangle."""
-        return self.width * self.height
-
-
-if __name__ == "__main__":
-
-    try:
-        Rectangle(10, "2")
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(10, 2)
-        r.width = -10
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        r = Rectangle(10, 2)
-        r.x = {}
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
-
-    try:
-        Rectangle(10, 2, 3, -1)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
+    def display(self):
+        """Displays the Rectangle instance with '#' characters."""
+        for _ in range(self.__height):
+            print("#" * self.__width)
