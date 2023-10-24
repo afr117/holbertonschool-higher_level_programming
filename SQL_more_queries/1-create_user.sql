943568c533ea3775cd3c69b37b9168ede6ec691c
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 08:49:47 -0700

    task1 edit

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,3 +3,7 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges to user_0d_1 on all databases
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+--SELECT IFNULL(COUNT(*), 0) as user_exists
+--FROM mysql.user
+--WHERE User = 'user_0d_1';
