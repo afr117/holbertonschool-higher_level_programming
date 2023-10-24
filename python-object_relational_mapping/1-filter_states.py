c94b0920e69e64a0b09d3f5d65f0e39fd6d6d6fe
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 14:31:09 -0700

    task 0 & 1 correct out for 0

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This script connects to a MySQL server and retrieves states with names starting with 'N'.
+
+Usage: ./1-filter_states.py <username> <password> <database>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        db.close()
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
+
