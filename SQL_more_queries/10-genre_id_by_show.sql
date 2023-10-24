dbb1404f315700ce33bdaa0cefef674d6d5bbcff
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 10:10:06 -0700

    task 10 edited format code correct out with comments failed checks

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,7 @@
-SELECT s.title as title, tv_genres.id as genre_id
-FROM tv_shows s
-JOIN tv_show_genres ON s.id = tv_show_genres.show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY s.title ASC, tv_genres.id ASC;
+-- List all shows contained in the database hbtn_0d_tvshows that have at least one genre linked.
+-- Display each record as tv_shows.title - tv_show_genres.genre_id
+-- Sort results in ascending order by tv_shows.title and tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
