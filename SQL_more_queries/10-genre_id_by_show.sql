e9279fd2b7f595f7043ff6dc77648fa0c7a79950
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 10:06:52 -0700

    correct out wrong code checkers

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
---  lists all shows contained in the database hbtn_0d_tvshows.
---  that have at least one genre linked
-SELECT s.title as title, g.id as genre_id
-FROM tv_shows s JOIN tv_show_genres g ON s.genre_id = g.id
-ORDER BY s.title ASC, g.id ASC;
+SELECT s.title as title, tv_genres.id as genre_id
+FROM tv_shows s
+JOIN tv_show_genres ON s.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY s.title ASC, tv_genres.id ASC;
