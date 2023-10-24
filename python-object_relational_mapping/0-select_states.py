3cce1979f61aec1b8b776daa33811d26672ad7c1
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 10:46:51 -0700

    task0 correct out

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    try:
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id")
+        results = cursor.fetchall()
+        for row in results:
+            print(row)
+        cursor.close()
+        db.close()
+    except Exception as e:
+        print("Error: {}".format(e))
+
