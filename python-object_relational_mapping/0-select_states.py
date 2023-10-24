64997621da8a76a0c369e9f6c274ed39a3ffd6ca
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 14:25:05 -0700

    task 0 different code update guidelines

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python3
+"""
+This script connects to a MySQL server and retrieves all states from a database.
+
+Usage: ./0-select_states.py <username> <password> <database>
+"""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
     try:
-        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+        db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+        )
         cursor = db.cursor()
         cursor.execute("SELECT * FROM states ORDER BY id")
         results = cursor.fetchall()
@@ -20,6 +30,6 @@ if __name__ == "__main__":
             print(row)
         cursor.close()
         db.close()
-    except Exception as e:
-        print("Error: {}".format(e))
+    except MySQLdb.Error as e:
+        print("MySQL Error: {}".format(e))
 
