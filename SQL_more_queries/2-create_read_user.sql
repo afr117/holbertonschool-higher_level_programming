499955efaac58c99f50ea58943ed5054a0525b62
afr117 <alfredo.figueroa07@yahoo.com>
2023-10-24 09:02:37 -0700

    task 2 error on 1 and 2 somewhat correct out

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- Create the user user_0d_2 if it doesn't exist and grant SELECT privilege
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
